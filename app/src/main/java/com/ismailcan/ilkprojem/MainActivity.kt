e2d0340e22f0b6426ff240c61d2ae080e0129827
İsmail Can Yıldırım <ismailcan5106@gmail.com>
2022-10-11 19:20:50 +0300

    Initial commit

---
+++ app/src/main/java/com/ismailcan/ilkprojem/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.ismailcan.ilkprojem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun degistir(view : View){
+
+        textView.text="merhaba android"
+
+    }
+
+
+
+
+}
